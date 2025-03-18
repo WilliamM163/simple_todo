e80070b3fd0f52edc9ad65e508fc56a2d5249c23
William Massey <william.massey.163@gmail.com>
2025-03-19 12:42:21 +1300

    Quick Fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./App.module.css";
 function Header() {
     return (
         <header className={styles.header}>
-            <img src="/todo_logo.svg" alt="Simple To Do Logo"/>
+            <img src="./todo_logo.svg" alt="Simple To Do Logo"/>
             <p>Simple Todo</p>
         </header>
     );
